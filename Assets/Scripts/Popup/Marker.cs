cc7dcabaed87507f628121bd1170bed3dc766909
yiuyu <217013277@stu.vtc.edu.hk>
2022-07-03 12:26:39 +0800

    add marker content script

---
+++ Assets/Scripts/Popup/Marker.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Marker : MonoBehaviour
+{
+	[TextArea(3,10)]
+	public string Content;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		Button button = GetComponent<Button>();
+		button.onClick.AddListener(() => {
+			Popup popup = PopupController.Instance.CreatePopup();
+			//Init popup with params (canvas, text, text(optional), text(optional), action(optional))
+			popup.Init(PopupController.Instance.MainCanvas,
+				Content
+				);
+		});
+    }
+}
